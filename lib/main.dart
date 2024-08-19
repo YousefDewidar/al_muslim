c30ad3dc5cc9a3320c3b2f94cdbe156523240e83
yousefdewidar <ymahmoud1213@gmail.com>
2024-08-20 00:38:48 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AlMuslim());
+}
+
+class AlMuslim extends StatelessWidget {
+  const AlMuslim({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'المسلم',
+      theme: ThemeData(),
+    );
+  }
+}
